cbeeb7745c4e5d2f13324a326913f5ae3c30320e
Amy Mather <minigirlgeek@gmail.com>
2020-02-15 16:45:06 +0000

    Test of algorithm to position bricks at intervals tangent to the path.

--- bezier_conversion.py
+++ bezier_conversion.py
@@ -1,3 +1,5 @@
+import math
+
 class Node:
 	"""a representation of a point within a bezier curve"""
 	def __init__(self, x, y):
@@ -64,8 +66,29 @@ class Bezier:
 		c3 = t**3*self.p[3].y
 		return c0 + c1 + c2 + c3
 
+
+
 test = Bezier("M79.8,80.3C315.2,331.6,345.2-122.6,81,157")
 
-for i in range(11):
-	t = i/10
+dt = 0.01
+
+svg = """<svg viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+  {0}
+</svg>"""
+
+resolution = 15
+
+for i in range(resolution+1):
+	t = i/resolution
+	x = test.B_x(t)
+	y = test.B_y(t)
+	conversion = 180 / math.pi
+	dy = test.B_y(t + dt) - test.B_y(t - dt)
+	dx = test.B_x(t + dt) - test.B_x(t - dt)
+	angle = math.atan2(dy,dx) * conversion
+	svg = svg.format(f'<rect width="10" height="20" style="fill:rgb(255,0,255);stroke-width:1;stroke:rgb(0,0,0)" x="{x}" y="{y}" transform="rotate({angle} {x} {y}) translate(-5 -10)" />' + '{0}')
+	svg = svg.format(f'<line x1="{test.B_x(t + dt)}" y1="{test.B_y(t + dt)}" x2="{test.B_x(t - dt)}" y2="{test.B_y(t - dt)}" stroke="black"/>' + '{0}')
+	print(test.B_x(t + dt), test.B_y(t + dt))
 	print(test.B_x(t), test.B_y(t))
+	print(test.B_x(t - dt), test.B_y(t - dt))
+print(svg)
