8aca49e45ec41aad7123d2b874632a8b6485e308
Amy Mather <minigirlgeek@gmail.com>
2020-02-15 17:09:24 +0000

    Attempt at even brick spacing.

--- bezier_conversion.py
+++ bezier_conversion.py
@@ -1,4 +1,4 @@
-import math
+import math, numpy
 
 class Node:
 	"""a representation of a point within a bezier curve"""
@@ -50,8 +50,6 @@ class Bezier:
 		for coord in coordinates:
 			self.p.append(Node(coord[0], coord[1]))
 
-		
-
 	def B_x(self, t):
 		c0 = (1 - t)**3*self.p[0].x
 		c1 = 3*(1 - t)**2*t*self.p[1].x
@@ -66,9 +64,35 @@ class Bezier:
 		c3 = t**3*self.p[3].y
 		return c0 + c1 + c2 + c3
 
+	def length_approximation(self, resolution):
+			self.length = 0
+			old_length = 0
+			self.t_map = {}
+			old_x = None
+			old_y = None
+			for i in range(resolution+1):
+				t = i/resolution
+				x = self.B_x(t)
+				y = self.B_y(t)
+				if old_y:
+					dy = y - old_y
+					dx = x - old_x
+					old_length = self.length
+					self.length += (dy**2 + dx**2) ** 0.5
+					map_range = numpy.arange(old_length, self.length, 0.01)
+					for index in map_range:
+						self.t_map[round(index,2)] = t
+				old_y = y
+				old_x = x
+				
+			return self.length
 
 
-test = Bezier("M79.8,80.3C315.2,331.6,345.2-122.6,81,157")
+fish = Bezier("M79.8,80.3C315.2,331.6,345.2-122.6,81,157")
+fish.length_approximation(40)
+print(fish.t_map)
+
+
 
 dt = 0.01
 
@@ -76,19 +100,20 @@ svg = """<svg viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
   {0}
 </svg>"""
 
-resolution = 15
+bricks = 15
+spacing = fish.length/bricks
 
-for i in range(resolution+1):
-	t = i/resolution
-	x = test.B_x(t)
-	y = test.B_y(t)
+for i in range(bricks+1):
+	try:
+		t = fish.t_map[round(i*spacing,2)]
+	except KeyError:
+		t = fish.t_map[round(i*spacing,2) - 0.01]
+	x = fish.B_x(t)
+	y = fish.B_y(t)
 	conversion = 180 / math.pi
-	dy = test.B_y(t + dt) - test.B_y(t - dt)
-	dx = test.B_x(t + dt) - test.B_x(t - dt)
+	dy = fish.B_y(t + dt) - fish.B_y(t - dt)
+	dx = fish.B_x(t + dt) - fish.B_x(t - dt)
 	angle = math.atan2(dy,dx) * conversion
 	svg = svg.format(f'<rect width="10" height="20" style="fill:rgb(255,0,255);stroke-width:1;stroke:rgb(0,0,0)" x="{x}" y="{y}" transform="rotate({angle} {x} {y}) translate(-5 -10)" />' + '{0}')
-	svg = svg.format(f'<line x1="{test.B_x(t + dt)}" y1="{test.B_y(t + dt)}" x2="{test.B_x(t - dt)}" y2="{test.B_y(t - dt)}" stroke="black"/>' + '{0}')
-	print(test.B_x(t + dt), test.B_y(t + dt))
-	print(test.B_x(t), test.B_y(t))
-	print(test.B_x(t - dt), test.B_y(t - dt))
+	svg = svg.format(f'<line x1="{fish.B_x(t + dt)}" y1="{fish.B_y(t + dt)}" x2="{fish.B_x(t - dt)}" y2="{fish.B_y(t - dt)}" stroke="black"/>' + '{0}')
 print(svg)
