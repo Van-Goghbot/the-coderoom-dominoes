9e48783f7392aec9cc23be2673c3d65bb8134733
Amy Mather <minigirlgeek@gmail.com>
2020-02-26 10:02:14 +0000

    Make back compatable with python 2 :((((

--- bezier_interpolation.py
+++ bezier_interpolation.py
@@ -16,15 +16,6 @@ def create_path(start_x, start_y, start_rot, end_x, end_y, end_rot):
 	angle_2 = end_rot
 	x2, y2 = (end_x, end_y)
 
-	svg = """<svg version="1.1" baseProfile="basic" id="Layer_1"
-		 xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="600px" height="250px"
-		 viewBox="0 0 120 50" xml:space="preserve">
-	  {0}
-	</svg>"""
-
-	brick_1 = f'<rect width="6" height="9" style="fill:rgb(255,106,5);stroke-width:1;stroke:rgb(0,0,0)" x="{x1}" y="{y1}" transform="rotate({angle_1} {x1} {y1}) translate(-3 -4.5)" />'
-	brick_2 = f'<rect width="6" height="9" style="fill:rgb(255,106,5);stroke-width:1;stroke:rgb(0,0,0)" x="{x2}" y="{y2}" transform="rotate({angle_2} {x2} {y2}) translate(-3 -4.5)" />'
-
 	svg = svg.format(brick_1 + '{0}')
 	svg = svg.format(brick_2 + '{0}')
 
@@ -47,16 +38,7 @@ def create_path(start_x, start_y, start_rot, end_x, end_y, end_rot):
 	P3_x = x2
 	P3_y = y2
 
-	line_1 = f'<line x1="{x1}" y1="{y1}" x2="{x1 + (handle_1_dx/2)}" y2="{y1 + (handle_1_dy/2)}" stroke="black" />'
-	line_2 = f'<line x1="{x2 - (handle_2_dx/2)}" y1="{y2 - (handle_2_dy/2)}" x2="{x2}" y2="{y2}" stroke="black" />'
-
-	path = f'<path fill="none" stroke="#000000" stroke-width="1.2587" stroke-miterlimit="10" d="M{P0_x},{P0_y}C{P1_x},{P1_y},{P2_x},{P2_y},{P3_x},{P3_y}"/>'
-
-	svg = svg.format(line_1 + '{0}')
-	svg = svg.format(line_2 + '{0}')
-	svg = svg.format(path+ '{0}')
-
-	brick_path = bezier_conversion.Bezier(f"M{P0_x},{P0_y}C{P1_x},{P1_y},{P2_x},{P2_y},{P3_x},{P3_y}")
+	brick_path = bezier_conversion.Bezier("M" + str(P0_x) + "," + str(P0_y) + "C" str(P1_x) + "," + str(P1_y) + "," str(P2_x) + "," + str(P2_y) + "," + str(P3_x) + "," + str(P3_y))
 	brick_path.length_approximation(150)
 
 	dt = 0.01
